79ca79645f42520c834a6f8740871d4a70922b9a
andrew.avinante <andrewavinante@gmail.com>
2020-09-19 19:02:22 -0400

    another test

--- wedding/resources/views/index.blade.php
+++ wedding/resources/views/index.blade.php
@@ -12,6 +12,35 @@
 
 @section('main-content')
 <div id='app'>
+    <nav class='navbar navbar-expand-lg sticky-top'>
+        <span class="navbar-brand d-lg-none">Andrew & Kimberly</span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <div class='collapse navbar-collapse' id="navbarNav">
+            <ul class="container navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" data-page='our-wedding' href='/'>OUR WEDDING</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-page='our-story' href='/our-story'>OUR STORY</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-center" data-page='hotel-rec' href='/hotel-recommendations'>HOTEL RECOMMENDATIONS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-page='bridal-party' href='/bridal-party'>BRIDAL PARTY</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-page='photos' href='/photos'>PHOTOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-page='rsvp' href='/rsvp'>RSVP</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div id='title-container' class='img-text-overlay'>
         <div>
             <h1 id='main-title' class='text-center'>@{{ pageTitle }}</h1>
@@ -28,7 +57,7 @@
     </div>
     <div id='title-cover-image' class='cover-image'></div>
     <div id='wedding-info' class='text-center'>
-        <img class='divider' src='img/divider.svg'/>
+        <img class='divider' src='img/divider.svg' />
         <div>
             <h1>The Wedding</h1>
             <p>@{{ weddingDate.format('LLLL') }}</p>
