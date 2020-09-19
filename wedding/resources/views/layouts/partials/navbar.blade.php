d5bdaa857733e779305a2892afd4b431ba65c2d6
andrew.avinante <andrewavinante@gmail.com>
2020-09-19 19:01:02 -0400

    Debug navbar

--- wedding/resources/views/layouts/partials/navbar.blade.php
+++ wedding/resources/views/layouts/partials/navbar.blade.php
@@ -1,10 +1,10 @@
-<nav class='navbar navbar-expand-lg'>
+<nav class='navbar navbar-expand-lg sticky-top'>
     <span class="navbar-brand d-lg-none">Andrew & Kimberly</span>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars"></i>
     </button>
 
-    <div class='collapse navbar-collapse' id="nav-content">
+    <div class='collapse navbar-collapse' id="navbarNav">
         <ul class="container navbar-nav mr-auto">
             <li class="nav-item">
                 <a class="nav-link" data-page='our-wedding' href='/'>OUR WEDDING</a>
@@ -26,4 +26,27 @@
             </li>
         </ul>
     </div>
+</nav>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
 </nav>
