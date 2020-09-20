a4040b6eebbc2ee238e439414c1a3d91441f0ef7
andrew.avinante <andrewavinante@gmail.com>
2020-09-19 20:27:18 -0400

    Finished front end

--- wedding/resources/views/our-story.blade.php
+++ wedding/resources/views/our-story.blade.php
@@ -1,1 +1,39 @@
 @extends('layouts.app')
+
+@section('page-css')
+<link rel='stylesheet' href='css/our-story.css'>
+@endsection
+
+@section('main-content')
+<div class="container">
+    <h1 class='text-center title'>Our Story</h1>
+    <h2>The Abridged Version (Andrew)</h2>
+    <p>
+        Bacon ipsum dolor amet ham pancetta pork ground round capicola. Turducken bacon hamburger cupim picanha venison ham andouille chicken landjaeger short ribs. Beef sausage meatball spare ribs pork loin chuck bacon shankle frankfurter turkey tongue pork fatback. Jowl bacon sirloin, kielbasa cupim short ribs ham hock t-bone tongue meatloaf capicola drumstick. Salami jowl pastrami prosciutto boudin pork loin bresaola leberkas shank shankle. Alcatra shank cow, swine chicken bacon t-bone tri-tip pork belly leberkas meatball kevin brisket buffalo.
+    </p>
+
+    <p>
+        Doner pastrami pork belly brisket alcatra ham boudin ground round capicola cupim tri-tip burgdoggen. Chislic beef jowl turkey. Pork chop turkey ham hock porchetta filet mignon leberkas pancetta tri-tip. Andouille chuck strip steak t-bone prosciutto drumstick pork belly bacon, salami capicola bresaola meatloaf. Prosciutto sausage burgdoggen salami ham filet mignon tongue short ribs pork belly strip steak pig chicken turducken. Beef ribs tongue pork, meatball ground round flank jowl chicken sausage tri-tip tail cow venison filet mignon shoulder.
+    </p>
+
+    <h2>The Extended Version (Kimberly)</h2>
+    <p>
+        Bacon ipsum dolor amet filet mignon boudin cow, tail rump kevin frankfurter shoulder salami bresaola. Shoulder porchetta fatback, ribeye beef corned beef pork loin meatball flank capicola. Tenderloin pork sirloin short ribs, corned beef swine pork loin boudin cow pork belly prosciutto tongue cupim salami turkey. Beef ribs doner turkey shankle cupim sirloin hamburger bresaola chislic short ribs rump ball tip. Spare ribs kielbasa ham jerky tail meatball kevin sausage. Pastrami buffalo corned beef beef biltong picanha kielbasa sausage rump.
+    </p>
+    <p>
+        Chicken chislic pig cow, cupim salami spare ribs rump strip steak burgdoggen kielbasa jerky. Kevin pork corned beef, shoulder filet mignon meatloaf ribeye jowl brisket ham hock tri-tip. Biltong bacon rump jerky flank tenderloin. Buffalo shoulder pancetta kevin, bacon venison jowl tail hamburger tongue meatloaf kielbasa strip steak burgdoggen bresaola. Shoulder pig burgdoggen, tenderloin alcatra pork chop frankfurter pork belly rump drumstick bacon picanha hamburger.
+    </p>
+
+    <p>
+        Andouille rump landjaeger ham hock, tenderloin pig short ribs pancetta short loin. Pancetta cow turkey swine, strip steak leberkas tail flank tri-tip picanha pastrami. Ribeye leberkas tenderloin pig spare ribs t-bone sausage shankle cow. Strip steak alcatra porchetta jerky kielbasa, ground round shoulder ball tip pork loin shank. Venison shankle jowl spare ribs bresaola leberkas. Pig pork chop short ribs, ribeye porchetta flank cow corned beef strip steak. Biltong tongue strip steak pork chop, fatback spare ribs pig pancetta short ribs.
+    </p>
+
+    <p>
+        Meatloaf kielbasa kevin strip steak, landjaeger frankfurter picanha cow alcatra ham boudin turducken swine spare ribs fatback. Boudin turducken leberkas, turkey cow shoulder pancetta pork loin picanha alcatra rump venison beef flank fatback. Ball tip jowl andouille shoulder. Flank capicola spare ribs shoulder, pork loin biltong meatloaf short ribs kevin. Tenderloin venison turducken, tail chicken ball tip pancetta pork belly shank tongue.
+    </p>
+
+    <p>
+        Venison doner hamburger biltong alcatra. Venison short loin andouille meatloaf cow porchetta, kielbasa flank short ribs bacon pork belly doner jerky capicola. Tongue meatball shank, turkey chuck porchetta venison ball tip kevin. Pork strip steak chuck, corned beef sausage cow tongue bresaola venison prosciutto jerky filet mignon. Swine doner cow brisket, sirloin ribeye landjaeger cupim pork belly filet mignon.
+    </p>
+</div>
+@endsection
