580c390a3a9907fbd78e0adc20fb2cb0beff335a
andrew.avinante <andrewavinante@gmail.com>
2020-11-30 21:34:53 -0500

    Hid our story tab

--- wedding/resources/views/layouts/partials/navbar.blade.php
+++ wedding/resources/views/layouts/partials/navbar.blade.php
@@ -9,9 +9,9 @@
             <li class="nav-item">
                 <a class="nav-link" data-page='our-wedding' href='/'>OUR WEDDING</a>
             </li>
-            <li class="nav-item">
+            <!-- <li class="nav-item">
                 <a class="nav-link" data-page='our-story' href='/our-story'>OUR STORY</a>
-            </li>
+            </li> -->
             <li class="nav-item">
                 <a class="nav-link text-center" data-page='hotel-rec' href='/hotel-recommendations'>HOTEL RECOMMENDATIONS</a>
             </li>
